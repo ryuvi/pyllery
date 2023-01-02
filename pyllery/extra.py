a621327b0f9819be05a2c748455fa767b4ac8a02
ryuvi <vicenters10@gmail.com>
2023-01-02 09:17:52 -0300

    Added More links

--- pyllery/extra.py
+++ pyllery/extra.py
@@ -1,14 +1,87 @@
 from os import listdir
-from os.path import join, isdir, isfile, basename
+from os.path import join, isdir, isfile, basename, splitext
+from pathlib import Path
 
 from PIL import Image, ImageTk
+from PIL.ImageFile import LOAD_TRUNCATED_IMAGES
 from tkinter import Label, filedialog, Tk
+from threading import Timer
 
+from pyaes import AESModeOfOperationCTR
 
+LOAD_TRUNCATED_IMAGES = True
 _file_list = []
-_curr_image = ''
+_curr_image = ""
+_old_size = (0, 0)
+_curr_img_size = (0, 0)
 _label: Label
 _win: Tk
+key = ''
+_is_from_search = False
+extensions = [
+    ".jpg",
+    ".jpeg",
+    ".jpe",
+    ".jif",
+    ".jfif",
+    ".jfi",
+    ".png",
+    ".gif",
+    ".webp",
+    ".tiff",
+    ".tif",
+    ".psd",
+    ".raw",
+    ".arw",
+    ".cr2",
+    ".nrw",
+    ".k25",
+    ".bmp",
+    ".dib",
+    ".heif",
+    ".heic",
+    ".ind",
+    ".indd",
+    ".indt",
+    ".jp2",
+    ".j2k",
+    ".jpf",
+    ".jpx",
+    ".jpm",
+    ".mj2",
+    ".svg",
+    ".svgz",
+    ".ai",
+    ".eps",
+    ".pdf",
+]
+_cur_theme = "classic"
+
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
+
+
+def is_valid_file(file) -> bool:
+    result = False
+    filename, extension = splitext(file)
+    if extension in extensions:
+        result = True
+
+    return result
+
+
+def check_list(file_list) -> None:
+    global _file_list
+
+    flist = []
+    for file in file_list:
+        if is_valid_file(file):
+            flist.append(file)
+
+    _file_list = flist
 
 
 def set_win(widget):
@@ -17,23 +90,24 @@ def set_win(widget):
 
 
 def search_files(folder):
-    global _file_list
+    global _file_list, _is_from_search
+    _is_from_search = True
     arr = []
     for item in listdir(folder):
         new_path = join(folder, item)
-        if isdir(item):
-            search_files(new_path)
-            
+
+        if isdir(new_path):
+            arr += search_files(new_path)
+
         else:
             arr.append(new_path)
 
-    _file_list = arr   
     return arr
 
 
 def have_file(folder):
     for item in listdir(folder):
-        if isfile(f'{folder}/{item}'):
+        if isfile(f"{folder}/{item}"):
             return True
         else:
             return False
@@ -47,27 +121,28 @@ def take_files(folder):
         if isfile(filepath):
             _files.append(filepath)
 
-    _file_list = _files
+    check_list(_files)
     return _files
 
 
 def open_file():
-    file = filedialog.askopenfilename()
-    take_files(file.replace(basename(file), ''))
+    file = filedialog.askopenfilename(initialdir=f"{Path.home()}")
+    take_files(file.replace(basename(file), ""))
     set_image(_label, file)
 
 
 def open_folder():
-    folder = filedialog.askdirectory()
+    folder = filedialog.askdirectory(initialdir=f"{Path.home()}")
 
     if have_file(folder):
         files = take_files(folder)
+        check_list(files)
         set_image(_label, files[0])
 
     else:
         files = search_files(folder)
+        check_list(files)
         set_image(_label, files[0])
-
 
 
 def set_label(widget):
@@ -80,43 +155,71 @@ def _win_size() -> tuple:
 
 
 def rule_of_three(total, target) -> float:
-    return ((total*target)/100)
+    return (total * target) / 100
+
+
+def take_bigger(size: tuple) -> float:
+    return size[0] if size[0] > size[1] else size[1]
+
+
+def take_percentage(usize, fsize) -> float:
+    return ((100 * usize) / fsize) / 100
+
+
+def get_key():
+    global key
+    with open(filedialog.askopenfilename(initialdir=f'{Path.home()}', title="Open Key"), 'rb') as r:
+        key = r.read()
 
 
 def set_image(widget: Label, image: str):
-    global _curr_image
+    global _curr_image, _curr_img_size
 
-    img = Image.open(image)
-    width, height = img.size
-    w_width, w_height = _win_size() if _win_size() > (1,1) else (600,400)
+    if (is_valid_file(image) is True) or (image.endswith('.broken')):
+        if image.endswith('.broken'):
+            if key in (None, ''):
+                get_key()            
+            with open(image, 'rb') as f:
+                _ = AESModeOfOperationCTR(key).decrypt(f.read())
+            with open(f'{Path.cwd()}/temp.temp', 'wb') as w:
+                w.write(_)
+            img_path = f'{Path.cwd()}/temp.temp'
+        else:
+            img_path = image
+        img = Image.open(img_path)
+        width, height = img.size
+        w_size = _win_size() if _win_size() > (1, 1) else (600, 400)
+        w_width, w_height = w_size
 
-    # Take 75% of the screen
-    usable_size = rule_of_three(w_width, 90)
+        # Take 90% of the screen
+        usable_size = rule_of_three(take_bigger(w_size), 90)
 
-    # Take percentage to be multiplied into the image size to resize it to a fitable size
-    ratio = ((100 * usable_size) / width) / 100
-    while height > w_height or width > w_width:
-        width *= ratio
-        height *= ratio
+        # Take percentage to be multiplied into the image size to resize it to a fitable size
+        ratio = take_percentage(usable_size, take_bigger(img.size))
+        while height > w_height or width > w_width:
+            width *= ratio
+            height *= ratio
 
-    size = (int(width), int(height))
+        size = (int(width), int(height))
 
-    img = img.resize(size)
-    img = ImageTk.PhotoImage(img)
-    
-    widget['image'] = img
-    widget.photo = img
+        _curr_img_size = size
+        img = img.resize(size)
+        img = ImageTk.PhotoImage(img)
 
-    _curr_image = image
+        widget["image"] = img
+        widget.photo = img
 
-    take_files(image.replace(basename(image), ''))
+        _curr_image = image
+
+        if _is_from_search is False:
+            take_files(image.replace(basename(image), ""))
 
 
 def left_key(key):
     global _curr_image
     old_idx = _file_list.index(_curr_image)
-    _curr_image = _file_list[old_idx-1]
-    
+    _curr_image = _file_list[old_idx - 1]
+
     set_image(_label, _curr_image)
 
 
@@ -127,5 +230,39 @@ def right_key(key):
     if new_idx >= len(_file_list):
         new_idx = 0
     _curr_image = _file_list[new_idx]
-    
+
     set_image(_label, _curr_image)
+
+
+def mouse_click(key):
+    global _curr_img_size, _old_size
+
+    img = Image.open(_curr_image)
+    width, height = img.size
+    ratio = take_percentage(
+        rule_of_three(take_bigger(img.size), 25), take_bigger(img.size)
+    )
+
+    if _curr_img_size > _win_size():
+        size = _old_size
+        _curr_img_size = size
+    else:
+        _old_size = _curr_img_size
+        size = (int(width / ratio), int(height / ratio))
+        _curr_img_size = size
+
+    img = img.resize(size)
+    img = ImageTk.PhotoImage(img)
+
+    _label["image"] = img
+    _label.photo = img
+
+
+_timer = RepeatTimer(2, right_key, args=("a",))
+
+
+def set_slideshow():
+    if _timer.is_alive():
+        _timer.cancel()
+    else:
+        _timer.start()
--- pyllery/main.py
+++ pyllery/main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 from sys import argv
-from tkinter import Menu, PhotoImage, Tk, Label
+from tkinter import Menu, Tk, Label
 from tkinter import CENTER
+from tkinter.ttk import Style
 from os.path import isdir, join, dirname
 
-from pyllery.extra import *
+from extra import *
 
 
 def init():
     root = Tk()
-    root.title('pyllery')
-    
+    root.title("pyllery")
+
     # TODO: Later add the icon (This shit does not work yet)
     # basedir = dirname(__file__)
     # root.iconphoto(True, PhotoImage(file=join(basedir, '..', 'icon.png')))
@@ -21,16 +22,22 @@ def init():
 
     files = []
 
+    style = Style(root)
+    style.theme_use("classic")
+
     label.place(anchor=CENTER, rely=0.5, relx=0.5)
     root.config(menu=menu)
-    root.geometry('800x600')
-    menu.add_command(label='<', command=lambda: left_key('a'))
-    menu.add_command(label='>', command=lambda: right_key('a'))
-    menu.add_command(label='Open File', command=open_file)
-    menu.add_command(label='Open Folder', command=open_folder)
+    root.geometry("800x600")
+    menu.add_command(label="<", command=lambda: left_key("a"))
+    menu.add_command(label=">", command=lambda: right_key("a"))
+    menu.add_command(label="Open File", command=open_file)
+    menu.add_command(label="Open Folder", command=open_folder)
+    menu.add_command(label="Slideshow", command=set_slideshow)
+    menu.add_command(label="Close", command=exit)
 
-    root.bind('<Left>', left_key)
-    root.bind('<Right>', right_key)
+    root.bind("<Left>", left_key)
+    root.bind("<Right>", right_key)
+    label.bind("<Double-Button-1>", mouse_click)
     set_label(label)
     set_win(root)
 
@@ -43,12 +50,12 @@ def init():
         elif isdir(dir) and not have_file(dir):
             files = search_files(dir)
             set_image(label, files[0])
-        
+
         else:
             set_image(label, dir)
 
-
     root.mainloop()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init()
